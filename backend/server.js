d9f91113ff53c6787344df47f77e0cb49df91930
Tyler Knapp <tgknapp11@gmail.com>
2020-01-31 11:11:09 -0500

    complete code

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true });
+mongoose.connect("mongodb://127.0.0.1:27017/tasks", { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once("open", function() {
